c16c9dfb2a65a7b72ff2ed63afd1a58d8e81bf33
iOSDev <fertech@yahoo.com>
2021-07-16 23:23:31 -0500

    Update and rename hero.js to Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const hero = () => {
+const Hero = () => {
     return (
         <div className="bg-white h-screen flex flex-col justify-center item-center">
 
-            <h1 className="lg:text-9xl md:text-7xl sm:text-5xltext-3xl font-black mb-14"></h1>
+            <h1 className="lg:text-9xl md:text-7xl sm:text-5xltext-3xl font-black mb-14">qanda</h1>
 
         </div>
     );
 };
 
-export default hero
+export default Hero
