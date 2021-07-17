30dbf06a345f17db34d6901669b8d2011a413b67
iOSDev <fertech@yahoo.com>
2021-07-16 21:10:26 -0500

    Update Navbar.js
    added link to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
     return (
-        <>
-            <h1 className='text-yellow-500'>Hello Ajmal</h1>
-        </>
-    )
-}
+        <nav className= "flex jutify-between items-center h-16 bg-white text-blackrelative shadow-sm font-mono" role="navigation">
+            <Link to="/" className="pl-8"> </Link>
+            <div className="px-4 cursor-pointer md:hidden">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
+            </div>
+            <div className="pr-8 md:block hidden">
+            <Link className="p-4" to="/Home"> Home</Link>
+            <Link className="p-4" to="/Service">About me</Link>
+            <Link className="p-4" to="/Price">Services</Link>
 
+            </div>
+        </nav>
+    );
+};        
 export default Navbar
