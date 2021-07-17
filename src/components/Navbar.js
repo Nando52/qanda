370086c99216c75722d87230a9572537df9ff479
iOSDev <fertech@yahoo.com>
2021-07-16 23:28:20 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,11 @@ const Navbar = () => {
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
             </div>
             <div className="pr-8 md:block hidden">
-            <Link className="p-4" to="/Home"> Home</Link>
-            <Link className="p-4" to="/Service">About me</Link>
-            <Link className="p-4" to="/Price">Services</Link>
+            <Link className="p-4" to="/qanda">qanda</Link>
+            <Link className="p-4" to="/About">About</Link>
+            <Link className="p-4" to="/Products">Products</Link>
+            <Link className="p-4" to="/Teams">Teams</Link>
+
 
             </div>
         </nav>
