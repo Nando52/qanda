7b75189298b39b466104452e83358d9b44cb0c54
iOSDev <fertech@yahoo.com>
2021-07-16 22:05:10 -0500

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,1 +1,13 @@
+import React from 'react'
 
+const hero = () => {
+    return (
+        <div className="bg-white h-screen flex flex-col justify-center item-center">
+
+            <h1 className="lg:text-9xl md:text-7xl sm:text-5xltext-3xl font-black mb-14"></h1>
+
+        </div>
+    );
+};
+
+export default hero
